51d73e64f0b8a0d492151da23efb1e8c5ca71199
jianyong830 <jianyong4297@gmail.com>
2026-01-02 21:30:54 +0800

    practice for interview test

--- src/main/java/com/codewithyong/seleniumtestng/TestNg.java
+++ src/main/java/com/codewithyong/seleniumtestng/TestNg.java
@@ -1,37 +1,82 @@
 package com.codewithyong.seleniumtestng;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestNg {
-    public String baseurl="http://www.demo.guru99.com/V4/";
+
     public WebDriver driver;
-    String driverpath="/home/chali/Downloads/geckodriver";
+
     @BeforeTest
     public void LaunchBrowser()
     {
-        driver=new FirefoxDriver();
-        driver.get(baseurl);
+
+        ChromeOptions options = new ChromeOptions();
+
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        prefs.put("profile.password_manager_leak_detection", false);
+
+        options.setExperimentalOption("prefs", prefs);
+        options.addArguments("--headless");
+        options.addArguments("--window-size=1920,1080"); // make sure element is visible
+        options.addArguments("--disable-gpu");
+
+        driver = new ChromeDriver(options);
+        // maximized the browser window
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/client/#/auth/login");
+        System.out.println("Title is: " + driver.getTitle());
     }
+
     @BeforeMethod
-    public void loginHomepage()
-    {
-        driver.findElement(By.name("uid")).sendKeys("****");
-        driver.findElement(By.name("password")).sendKeys("****");
-        driver.findElement(By.name("btnLogin")).click();
+    public void loginHomepage() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(
+                ExpectedConditions.elementToBeClickable(By.id("userEmail"))
+        );
+        driver.findElement(By.id("userEmail")).sendKeys("rahulshetty@gmail.com");
+        driver.findElement(By.id("userPassword")).sendKeys("Iamking@000");
+        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id("login")));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", button);
+
+        // Wait a bit for overlay/animations
+        Thread.sleep(500);
+
+        try {
+            button.click();
+        } catch (org.openqa.selenium.ElementClickInterceptedException e) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", button);
+        }
     }
+
     @Test
     public void verifyHomepage()
     {
-        String ActualURL=driver.getCurrentUrl();
-        String ExpectedURL="http://www.demo.guru99.com/V4/manager/Managerhomepage.php";
-        Assert.assertEquals(ActualURL,ExpectedURL);
+        // Explicit wait for logo
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement logo = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".logo"))
+        );
+
+        // Assertion
+        Assert.assertTrue(logo.isDisplayed(), "Logo is not displayed after login");
     }
 
     @AfterTest
